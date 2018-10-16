e43326ef8188dd98a7d7b917d553a96749454654
thethinur <thinurtorsson@gmail.com>
2018-10-16 12:01:02 +0200

    Manual push

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,27 @@
-// Copyright 2018 Thinur Geir Torsson, Benjamin Badstue.
+/* Copyright (c) 2018 AARHUS TECH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+// Author: Thinur Geir Torsson
 // Snake game snake.js
-// Regular global vars
+// Documentation status: Partial, low (~20%)
+
+// Regular global named objects
 sprites = new Image();
 partnames = ['Head','Body','Tail1','Tail2'];
 // test vars
@@ -15,7 +36,7 @@ var GameArea = {
   },
   preload : function() {
     sprites.onload = function() {
-      ui.startscreen.image.onload = sprites.onload = function() {
+      ui.startscreen.image.onload = function() {
         GameArea.create();
         var g = setInterval(function() {
           GameArea.updateGraphics();
@@ -43,7 +64,7 @@ var GameArea = {
     ui.startscreen.active = true;
     ui.scorecounter.location.y = this.canvas.height - 50;
     score.reset();
-    snake.create([{x : 0, y : 4},{x : 0, y : 0},{x : 4, y : 0}]);
+    snake.create([{x : 1, y : 4},{x : 1, y : 1},{x : 4, y : 1}]);
   },
   start : function() {
 
